62026f02ae4f305b5490f501d2403fa4440b95e2
Marat K <yadeforz@gmail.com>
2025-03-03 23:50:26 +0300

    userservice with keycloak

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,37 +1,60 @@
 using Microsoft.EntityFrameworkCore;
 using EFCore.NamingConventions;
 using UserService.Data;
+using UserService.Extensions;
 using Prometheus;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddMetrics();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGenWithAuth(builder.Configuration);
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o=>
+    {
+        o.RequireHttpsMetadata = false;
+        o.Audience = builder.Configuration["Authentication:Audience"];
+        o.MetadataAddress = builder.Configuration["Authentication:MetadataAddress"];
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = builder.Configuration["Authentication:ValidIssuer"]
+        };
+    });
+
 builder.Services.AddDbContext<AppDbContext>(options => options
     .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
     .UseSnakeCaseNamingConvention());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (true)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//app.UseAuthorization();
 
 app.MapControllers();
 
 app.MapGet("/health/", () => Results.Json(new { status = "OK" }));
+app.MapGet("/users/me", (ClaimsPrincipal claimsPrincipal) =>
+{
+    return claimsPrincipal.Claims.ToDictionary(c => c.Type, c => c.Value);
+}).RequireAuthorization();
 
 app.UseRouting();
-app.UseHttpMetrics(); // Add HTTP metrics
-app.MapMetrics(); // Map the Prometheus endpoint at /metrics
-app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseHttpMetrics(); 
+app.MapMetrics(); 
+
+//app.MapControllers();
 
 app.Run();
